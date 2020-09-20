f4b70a0f12b56914e6668d678fbcbc0a2aaa21f7
mharrvic <mharrvic@gmail.com>
2020-09-20 16:52:40 +0800

    shadow hide

---
+++ src/CustomDragLayer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDragLayer, XYCoord } from "react-dnd";
+import { Column } from "./Column";
+import { CustomDragLayerContainer } from "./styles";
+
+function getItemStyles(currentOffset: XYCoord | null): React.CSSProperties {
+  if (!currentOffset) {
+    return {
+      display: "none",
+    };
+  }
+  const { x, y } = currentOffset;
+  const transform = `translate(${x}px, ${y}px)`;
+  return {
+    transform,
+    WebkitTransform: transform,
+  };
+}
+
+export const CustomDragLayer: React.FC = () => {
+  const { isDragging, item, currentOffset } = useDragLayer((monitor) => ({
+    item: monitor.getItem(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging(),
+  }));
+
+  return isDragging ? (
+    <CustomDragLayerContainer>
+      <div style={getItemStyles(currentOffset)}>
+        <Column id={item.id} text={item.text} index={item.index} />
+      </div>
+    </CustomDragLayerContainer>
+  ) : null;
+};
